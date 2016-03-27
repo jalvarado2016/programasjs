947bb83bc0089776ce978643d8fe139feda35d32
John <jois.alvanez@gmail.com>
2016-03-27 12:31:51 -0500

    se hicieron nuevos archivos y el programa de juego de dados x3

---
+++ randon.js
@@ -0,0 +1,9 @@
+console.log("Inicio del programa");
+function tiraDados(){
+var dado = Math.floor(Math.random() * 6) + 1;
+alert("Dado dice:" + dado);}
+alert("Tiramos 3 dados: ");
+tiraDados();
+tiraDados();
+tiraDados(); 
+console.log("Programa completado");
